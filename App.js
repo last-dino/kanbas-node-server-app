9c58a1194cbf43f8fa65acf984bbcc765fb24792
Dongjun Xie <joycexie0615@gmail.com>
2024-08-05 12:23:38 -0700

    node for quiz list screen

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import QuizRoutes from "./Kanbas/Quizzes/routes.js";
 import cors from "cors";
 import session from "express-session";
 
@@ -38,6 +39,7 @@ UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
+QuizRoutes(app)
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000)
---
+++ Kanbas/Database/quizzes.js
@@ -0,0 +1,130 @@
+[
+  {
+    "title": "Q1 - Introduction to Rocket Propulsion",
+    "course": "RS101",
+    "available": "Closed",
+    "due": "Aug 2 at 1pm",
+    "points": "30 pts",
+    "questions": "10 questions"
+  },
+  {
+    "title": "Q2 - Types of Rocket Engines",
+    "course": "RS101",
+    "available": "Closed",
+    "due": "Aug 9 at 1pm",
+    "points": "15 pts",
+    "questions": "3 questions"
+  },
+  {
+    "title": "EXAM 1 SM2 24",
+    "course": "RS101",
+    "available": "Closed",
+    "due": "Aug 16 at 5:30pm",
+    "points": "100 pts",
+    "questions": "20 questions"
+  },
+  {
+    "title": "Q3 - Combustion Processes in Rocket Engines",
+    "course": "RS101",
+    "available": "Avilable Multiple Dates",
+    "due": "Multiple Dates",
+    "points": "20 pts",
+    "questions": "5 questions"
+  },
+  {
+    "title": "Q4 - Rocket Fuel and Combustion",
+    "course": "RS101",
+    "available": "Avilable Multiple Dates",
+    "due": "Multiple Dates",
+    "points": "35 pts",
+    "questions": "5 questions"
+  },
+  {
+    "title": "Q5 - Combustion Processes in Rocket Engines",
+    "course": "RS101",
+    "available": "Not available until sep 1 at 3:00pm",
+    "due": "Sep 8 at 1pm",
+    "points": "10 pts",
+    "questions": "2 questions"
+  },
+  {
+    "title": "Q6 - Nozzle Design and Performance",
+    "course": "RS101",
+    "available": "Not available until sep 8 at 3:00pm",
+    "due": "Sep 15 at 1pm",
+    "points": "20 pts",
+    "questions": "2 questions"
+  },
+  {
+    "title": "EXAM 2 SM2 24",
+    "course": "RS101",
+    "available": "Not available until Sep 15 at 3:00pm",
+    "due": "Sep 15 at 8:00pm",
+    "points": "104 pts",
+    "questions": "18 questions"
+  },
+  {
+    "title": "Q1 - Introduction to Rocket Propulsion",
+    "course": "RS102",
+    "available": "Closed",
+    "due": "Aug 2 at 1pm",
+    "points": "30 pts",
+    "questions": "10 questions"
+  },
+  {
+    "title": "Q2 - Types of Rocket Engines",
+    "course": "RS102",
+    "available": "Closed",
+    "due": "Aug 9 at 1pm",
+    "points": "15 pts",
+    "questions": "3 questions"
+  },
+  {
+    "title": "EXAM 1 SM2 24",
+    "course": "RS102",
+    "available": "Closed",
+    "due": "Aug 16 at 5:30pm",
+    "points": "100 pts",
+    "questions": "20 questions"
+  },
+  {
+    "title": "Q3 - Combustion Processes in Rocket Engines",
+    "course": "RS102",
+    "available": "Closed",
+    "due": "Aug 23 at 1pm",
+    "points": "20 pts",
+    "questions": "5 questions"
+  },
+  {
+    "title": "Q4 - Rocket Fuel and Combustion",
+    "course": "RS102",
+    "available": "Closed",
+    "due": "Sep 1 at 1pm",
+    "points": "35 pts",
+    "questions": "5 questions"
+  },
+  {
+    "title": "Q5 - Combustion Processes in Rocket Engines",
+    "course": "RS102",
+    "available": "Not available until sep 1 at 3:00pm",
+    "due": "Sep 8 at 1pm",
+    "points": "10 pts",
+    "questions": "2 questions"
+  },
+  {
+    "title": "Q6 - Nozzle Design and Performance",
+    "course": "RS102",
+    "available": "Not available until sep 8 at 3:00pm",
+    "due": "Sep 15 at 1pm",
+    "points": "20 pts",
+    "questions": "2 questions"
+  },
+  {
+    "title": "EXAM 2 SM2 24",
+    "course": "RS102",
+    "available": "Not available until Sep 15 at 3:00pm",
+    "due": "Sep 15 at 8:00pm",
+    "points": "104 pts",
+    "questions": "18 questions"
+  }
+]
